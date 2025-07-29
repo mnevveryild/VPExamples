567a6dce4e98c0e8d015eab3a5feabe160ab4b60
Münevver Yıldırım <mnevveryldrm01@gmail.com>
2025-07-29 15:21:54 +0300

    İlk yükleme

---
+++ Calisma2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calisma2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14159;  // sabit değer
+            
+            double r;
+            Console.WriteLine("Enter Radius: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            double areaCircle = pi * r * r;
+            Console.WriteLine("Radius: {0}, Area: {1}", r, areaCircle);
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
