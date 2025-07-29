f6b5839579492956ba953751b7286e12974c0f95
Münevver Yıldırım <mnevveryldrm01@gmail.com>
2025-07-29 15:22:58 +0300

    ilk yükleme

---
+++ Calisma3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calisma3
+{
+    internal class Program
+    {
+        class Bread
+        {
+            public int counter;
+        }
+
+        static class Butter
+        {
+            public static int counter;
+        }
+        static void Main(string[] args)
+        {
+            
+            Bread a = new Bread();
+            a.counter = 5;
+            Console.WriteLine("Bread: " + a.counter);    // örnek gerekli
+
+           
+            Butter.counter = 7;
+            Console.WriteLine("Butter: " + Butter.counter); // örnek gerekmez
+
+        }
+    }
+}
